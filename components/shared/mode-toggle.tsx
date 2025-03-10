5734755de606ae6c4a007c0944452d19cd17fae0
Skandar Souissi <iskandarsouissi.0@gmail.com>
2025-03-10 08:56:17 +0100

    Updated the color of toggles when one is selected.

--- components/shared/mode-toggle.tsx
+++ components/shared/mode-toggle.tsx
@@ -19,39 +19,35 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="size-8 px-4">
-          <Icons.sun className="absolute size-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
-          <Icons.moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Icons.sun className="absolute size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90" />
+          <Icons.moon className="absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className={"space-y-1"}>
         <DropdownMenuItem
-          className={
-            theme === "light" ? "bg-primary text-primary-foreground" : ""
-          }
+          className={theme === "light" ? "bg-accent accent-foreground" : ""}
           onClick={() => setTheme("light")}
         >
           <span className="sr-only">Toggle light theme</span>
           <Icons.sun className="mr-2 size-4" />
-          Light
+          <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem
-          className={theme === "dark" ? "bg-primary" : ""}
+          className={theme === "dark" ? "bg-accent accent-foreground" : ""}
           onClick={() => setTheme("dark")}
         >
           <span className="sr-only">Toggle dark theme</span>
           <Icons.moon className="mr-2 size-4" />
-          Dark
+          <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem
-          className={
-            theme === "system" ? "bg-primary text-primary-foreground" : ""
-          }
+          className={theme === "system" ? "bg-accent accent-foreground" : ""}
           onClick={() => setTheme("system")}
         >
           <span className="sr-only">Toggle system theme</span>
           <Icons.sunMoon className="mr-2 size-4" />
-          System
+          <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
